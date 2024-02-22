10af44655e1ccde5676ee2e6659f0510b5153c07
Yannisse Abbad <yannisse.abbad@epitech.eu>
2024-02-22 14:25:50 +0100

    init flask app

---
+++ index.py
@@ -0,0 +1,14 @@
+# from flask import Flask
+
+# app = Flask(__name__)
+
+
+# @app.route('/')
+# def home():
+    
+#     return render_template('index.html', name=name)
+#     return 'TEST APPLICATION !'
+
+
+# @app.route('/search-text', methods=['GET', 'POST'])
+# def search_txt()
